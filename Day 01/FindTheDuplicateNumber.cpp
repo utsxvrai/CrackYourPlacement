fcea46eaa3c934c22bbc7750676473d7433b8adc
Utsav Rai <utsavrai2115@gmail.com>
2024-07-16 00:27:22 +0530

    Initial commit

---
+++ Day 01/FindTheDuplicateNumber.cpp
@@ -0,0 +1,24 @@
+// TakeAway: Floyd's Cycle Detection Algorithm
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+// https://leetcode.com/problems/find-the-duplicate-number/
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        
+        while(slow!=fast){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
+
